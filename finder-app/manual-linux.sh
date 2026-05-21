db02e3cd6b01acf79b1b0f0ec0a8ef384b0ed1d2
H4nr1s3ld0n <p3pit0@tutamail.com>
2026-05-21 09:03:26 +0000

    Update manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,7 +11,7 @@ KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=/home/admin/aarch64-none-linux-gnu/bin
+CROSS_COMPILE=/home/admin/aarch64-none-linux-gnu-/bin
 COMPILER_DIR=/home/admin/compiler/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu
 
 if [ $# -lt 1 ]
