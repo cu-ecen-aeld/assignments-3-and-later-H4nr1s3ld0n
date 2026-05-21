bfe5ce9947c04a718ccffb8ae083908956abcab9
H4nr1s3ld0n <p3pit0@tutamail.com>
2026-05-21 08:10:52 +0000

    Update manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+COMPILER_DIR=/home/admin/compiler/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu
 
 if [ $# -lt 1 ]
 then
@@ -35,6 +36,11 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +54,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -61,20 +70,31 @@ else
 fi
 
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp -a ${COMPILER_DIR}/lib/*.so* ${OUTDIR}/usr/lib64/ 2>/dev/null 
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
 
 # TODO: Clean and build the writer utility
+make clean writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp /home/admin/embedded_assignments/finder-app ${OUTDIR}/home/finder-app
 
 # TODO: Chown the root directory
+cd "${OUTDIR}"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 
 # TODO: Create initramfs.cpio.gz
+gzip -f initramfs.cpio 
